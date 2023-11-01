dd274de8626a49b53debf40895ad95035d467750
문소희 <thgmlxn@hanmail.net>
2023-11-01 21:28:33 +0900

    임베디드 타입, 값 타입

--- src/main/java/jpabook/shop/JpaMain.java
+++ src/main/java/jpabook/shop/JpaMain.java
@@ -4,10 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import jpabook.shop.domain.Member;
-import jpabook.shop.domain.Movie;
-import jpabook.shop.domain.Order;
-import jpabook.shop.domain.Team;
+import jpabook.shop.domain.*;
 
 import java.util.List;
 
@@ -21,17 +18,26 @@ public class JpaMain {
         try {
            Member member = new Member();
            member.setName("소희");
+           //값 타입 컬렉션 사용하기 => 라이프 사이클이 member를 따라감.
+           member.setHomeAddress(new Address("city1","street","zipcode"));
+           member.getFavoriteFoods().add("치킨");
+           member.getFavoriteFoods().add("피자");
+           member.getFavoriteFoods().add("곱창");
+
+           member.getAddressHistory().add(new Address("city2","street2","zipcode2"));
+           member.getAddressHistory().add(new Address("city3","street3","zipcode3"));
            em.persist(member);
 
            em.flush();
            em.clear();
 
-            //Member findMsember = em.find(Member.class, member.getId());
+            Member fmember = em.find(Member.class,member.getId());
 
-            Member findMsember = em.getReference(Member.class, member.getId());
-            //getReference 는 실제 사용되는 시점에 조회를 함.
-            System.out.println("findMember" + findMsember.getName());
+            fmember.getFavoriteFoods().remove("치킨");
+            fmember.getFavoriteFoods().add("한식");
 
+            fmember.getAddressHistory().remove(new Address("city2","street2","zipcode2"));
+            fmember.getAddressHistory().add(new Address("citycity","streettreet","zipcodecode"));
 
 
             tx.commit();
---
+++ src/main/java/jpabook/shop/domain/Period.java
@@ -0,0 +1,22 @@
+package jpabook.shop.domain;
+
+import jakarta.persistence.Embeddable;
+
+import java.time.LocalDateTime;
+@Embeddable
+public class Period {
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public Period(){
+
+    }
+}
